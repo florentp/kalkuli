d18b061926174e413d60836b2f03c09ddc6edd1f
Florent Paillard <florent.paillard@gmail.com>
2011-01-01 22:34:03 +0100

    Removing PersonQuery::formOptionsArray which is not used since a few revisions

--- classes/PersonQuery.php
+++ classes/PersonQuery.php
@@ -15,17 +15,4 @@
  */
 class PersonQuery extends BasePersonQuery {
 
-	public static function formOptionsArray () {
-		
-		$peopleList = self::create()
-			->orderByPersonname()
-			->find();
-
-		$peopleOptions = array();
-		foreach($peopleList as $person)
-			$peopleOptions[$person->getPersonId()] = $person->getPersonName();
-		
-		return $peopleOptions;
-	}
-
 } // PersonQuery
