45fef3b90989656dccafa2f10251789d3970d372
Florent Paillard <florent.paillard@gmail.com>
2011-08-15 04:21:13 +0200

    Add GPL v3 license information

--- kalkuli/classes/KeyGenerator.php
+++ kalkuli/classes/KeyGenerator.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * Copyright 2006-2011 Florent Paillard
+ * 
+ * This file is part of /kal.ku.'li/.
+ * 
+ * /kal.ku.'li/ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * /kal.ku.'li/ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with /kal.ku.'li/.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 	class KeyGenerator {
 		
 		private $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
