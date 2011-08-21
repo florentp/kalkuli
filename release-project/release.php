9364a6d7196a2658951d93280c613ccdb2264a0c
Florent Paillard <florent.paillard@gmail.com>
2011-08-21 18:39:23 +0200

    Improve release script

--- release-project/release.php
+++ release-project/release.php
@@ -42,18 +42,31 @@ $zip = new CustZipArchive();
 $zip->open($archiveFilename, ZIPARCHIVE::CREATE);
 
 $excludeFileset = array(
-		'.buildpath',
-		'.htaccess',
-		'.project',
-		'.settings',
-		'include/config.inc.php',
-		'propel-project'
+		'kalkuli/.buildpath',
+		'kalkuli/.htaccess',
+		'kalkuli/.project',
+		'kalkuli/.settings',
+		'kalkuli/include/config.inc.php',
+		'kalkuli/templates_c',
+		'kalkuli/propel-project'
 	);
 $zip->addDir('kalkuli', $excludeFileset);
+$zip->addEmptyDir('kalkuli/templates_c');
 
 $zip->close();
 
 echo $archiveFilename . "\n";
+
+function startsWith($haystack, $needle) {
+	$length = strlen($needle);
+	return (substr($haystack, 0, $length) === $needle);
+}
+
+function endsWith($haystack, $needle) {
+	$length = strlen($needle);
+	$start  = $length * -1; //negative
+	return (substr($haystack, $start) === $needle);
+}
 
 function rm($path) {
 	if (is_dir($path)) {
@@ -78,17 +91,20 @@ function rm($path) {
 class CustZipArchive extends ZipArchive {
 	public function addDir($path, $excludeFileset) {
 		$this->addEmptyDir($path);
-		$nodes = glob($path . '/*');
-		if (isset($concatPath))
-			$concatPath .= '/' . $path;
-		else
-			$concatPath = '';
+		$nodes = glob("$path/{,.}*", GLOB_BRACE);
 		
 		foreach ($nodes as $node) {
-			if (is_dir($node))
-				$this->addDir($node, $excludeFileset);
-			else if (is_file($node) && !in_array($concatPath . '/' . $node, $excludeFileset))
-				$this->addFile($node);
+			if (!endsWith($node, '/.')
+					&& !endsWith($node, '/..')
+					&& !in_array($node, $excludeFileset)) {
+				
+				if (is_dir($node)) {
+					$this->addDir($node, $excludeFileset);
+				}
+				else if (is_file($node)) {
+					$this->addFile($node);
+				}
+			}
 		}
 	}
 }
